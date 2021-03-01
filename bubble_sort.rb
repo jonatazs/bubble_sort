1498f9782de3f9bad4aefa22f8af24ff6e0095bc
Jônatas Vasconcelos <68757844+jonatazs@users.noreply.github.com>
2021-03-01 00:50:25 -0300

    Add files via upload

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,24 @@
+
 def bubble_sort(array)
-    #Store the sorted values
-    sorted_array = []
-    #Loop counter
-    i = 0
-    #Validation to continue looping
-    while array[i + 1] < array.length
-        #Verify if the number on the actual index is greater or smaller than the next one
-        if array[i] >= array[i+1]
-            sorted_array.push(array[i])
-        else
-            sorted_array.unshift(array[i])
-        end
-        #Increase the looper
-        i += 1
-    end 
-
-   puts "#{array.sort}"
-
-
+	array.each do |single|
+		i = 0
+ 		
+ 		while i < (array.size - 1)
+ 			if array[i] > array[i + 1]
+ 				#puts "#{array}"
+ 				array[i], array[i + 1] = array[i +1], array[i]
+ 				
+ 			end
+			
+ 			i += 1
+			
+ 		end
+	end
+	print array
+	
 end
+
+
 bubble_sort([4,3,78,2,0,2])
 
 
